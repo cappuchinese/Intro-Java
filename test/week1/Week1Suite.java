24ab46181dd5958539023ab639bf5b9503b5a29d
MichielNoback <m.a.noback@pl.hanze.nl>
2015-04-15 20:09:27 +0200

    minor edit to suite

--- test/week1/Week1Suite.java
+++ test/week1/Week1Suite.java
@@ -12,7 +12,7 @@ import org.junit.runners.Suite;
  * @author michiel
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({week1_1.BmiCalculatorTest.class, week1_2.AllSubstringsPrinter.class, week1_3.WeightUnitsSolverTest.class})
+@Suite.SuiteClasses({week1_1.BmiCalculatorTest.class, week1_2.AllSubstringsPrinterTest.class, week1_3.WeightUnitsSolverTest.class})
 public class Week1Suite {
     
 }

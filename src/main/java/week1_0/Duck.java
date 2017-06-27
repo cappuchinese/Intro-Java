c9f1e3ce2c71f2c36bc645badbb44cba189fb7ae
Michiel Noback <m.a.noback@pl.hanze.nl>
2017-06-27 13:02:07 +0200

    Finished week1_0 assignments

---
+++ src/main/java/week1_0/Duck.java
@@ -0,0 +1,12 @@
+package week1_0;
+
+/**
+ * Creation date: Jun 27, 2017
+ *
+ * @version 0.01
+ * @autor Michiel Noback (&copy; 2017)
+ */
+public class Duck {
+    public int swimSpeed;
+    public String name;
+}
